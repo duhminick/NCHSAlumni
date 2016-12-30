79fe5b957c1dddeeee690e9f8f381aad3192c3da
Dominic <dominic@duhminick.com>
2016-12-30 12:27:08 -0500

    added VueRouter

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 
-var app = new Vue({
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: { template: '<div>Index</div>' } },
+  { path: '/login', component: { template: '<div>Login</div>' } }
+]
+const router = new VueRouter({ routes })
+
+let app = new Vue({
   el: '#app',
-  data: {
-    message: 'Testing 123'
-  }
+  router
 })
